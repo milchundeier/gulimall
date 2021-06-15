901818a4e46743051ddf69b14a1843433c04e700
Eagle <lzyphil@gmail.com>
2021-06-15 11:45:19 +0200

    init gulimall project

---
+++ gulimall-member/src/test/java/com/milchundeier/gulimall/memeber/GulimallMemeberApplicationTests.java
@@ -0,0 +1,13 @@
+package com.milchundeier.gulimall.memeber;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GulimallMemeberApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
