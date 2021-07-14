be5d3cb0c920914765f534d4330579e5c0593d45
Eagle <lzyphil@gmail.com>
2021-07-14 18:21:19 +0200

    45

--- gulimall-product/src/main/java/com/milchundeier/gulimall/product/controller/CategoryController.java
+++ gulimall-product/src/main/java/com/milchundeier/gulimall/product/controller/CategoryController.java
@@ -1,20 +1,16 @@
 package com.milchundeier.gulimall.product.controller;
 
-import java.util.Arrays;
-import java.util.Map;
-
+import com.milchundeier.common.utils.R;
+import com.milchundeier.gulimall.product.entity.CategoryEntity;
+import com.milchundeier.gulimall.product.service.CategoryService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.milchundeier.gulimall.product.entity.CategoryEntity;
-import com.milchundeier.gulimall.product.service.CategoryService;
-import com.milchundeier.common.utils.PageUtils;
-import com.milchundeier.common.utils.R;
-
+import java.util.Arrays;
+import java.util.List;
 
 
 /**
@@ -33,11 +29,12 @@ public class CategoryController {
     /**
      * 列表
      */
-    @RequestMapping("/list")
-    public R list(@RequestParam Map<String, Object> params){
-        PageUtils page = categoryService.queryPage(params);
+    @RequestMapping("/list/tree")
+    public R list(){
 
-        return R.ok().put("page", page);
+        List<CategoryEntity> entities = categoryService.listWithTree();
+
+        return R.ok().put("data", entities);
     }
 
 
--- gulimall-product/src/main/java/com/milchundeier/gulimall/product/service/impl/CategoryServiceImpl.java
+++ gulimall-product/src/main/java/com/milchundeier/gulimall/product/service/impl/CategoryServiceImpl.java
@@ -1,16 +1,19 @@
 package com.milchundeier.gulimall.product.service.impl;
 
-import org.springframework.stereotype.Service;
-import java.util.Map;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.milchundeier.common.utils.PageUtils;
 import com.milchundeier.common.utils.Query;
-
 import com.milchundeier.gulimall.product.dao.CategoryDao;
 import com.milchundeier.gulimall.product.entity.CategoryEntity;
 import com.milchundeier.gulimall.product.service.CategoryService;
+import org.springframework.stereotype.Service;
+
+import java.util.Comparator;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 
 @Service("categoryService")
@@ -24,6 +27,36 @@ public class CategoryServiceImpl extends ServiceImpl<CategoryDao, CategoryEntity
         );
 
         return new PageUtils(page);
+    }
+
+    @Override
+    public List<CategoryEntity> listWithTree() {
+        //1、查出所有分类
+        List<CategoryEntity> entities = baseMapper.selectList(null);
+
+        //2、组装成父子的树形结构
+
+        //2.1）、找到所有的一级分类
+        List<CategoryEntity> level1Menus = entities.stream().filter(categoryEntity ->
+                categoryEntity.getParentCid() == 0
+        ).map((menu)->{
+            menu.setChildren(getChildren(menu,entities));
+            return menu;
+        }).sorted(Comparator.comparingInt(menu -> (menu.getSort() == null ? 0 : menu.getSort()))).collect(Collectors.toList());
+
+        return level1Menus;
+    }
+
+    //递归查找所有菜单的子菜单
+    private List<CategoryEntity> getChildren(CategoryEntity root,List<CategoryEntity> all){
+
+        List<CategoryEntity> children = all.stream().filter(categoryEntity -> categoryEntity.getParentCid().equals(root.getCatId())).map(categoryEntity -> {
+            //1、找到子菜单
+            categoryEntity.setChildren(getChildren(categoryEntity,all));
+            return categoryEntity;
+        }).sorted(Comparator.comparingInt(menu -> (menu.getSort() == null ? 0 : menu.getSort()))).collect(Collectors.toList());
+
+        return children;
     }
 
 }
